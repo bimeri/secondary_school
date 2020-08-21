d76055e3873fa507462fcb86b42a1387b0c2a798
Bimeri@89 <bimeri.noel@go-groups.net>
2020-08-21 10:47:43 +0100

    ranking final

--- resources/views/admin/student_marks/rank_final.blade.php
+++ resources/views/admin/student_marks/rank_final.blade.php
@@ -59,6 +59,18 @@
     input[type=number]{
         -moz-appearance: textfield !important;
     }
+    .row .col.m7 {
+    width: 59.3333333333% !important;
+    margin-left: auto;
+    left: auto;
+    right: auto;
+  }
+  .row .col.m12 {
+    width: 98% !important;
+    margin-left: auto;
+    left: auto;
+    right: auto;
+  }
 </style>
 @endsection
 @section('content')
@@ -94,59 +106,113 @@
             </div>
         </form>
     </div>
-    <div class="col s11 m10 w3-border-t offset-m1 radius white w3-margin-left">
+    @if(Session::has('message'))
+    <div class="row">
+        <div class="col s12 m6 offset-m3 w3-center alert alert-danger w3-padding" role="alert" style="height: 70px">
+            {{ Session::get('message') }}
+        </div>
+    </div>
+
+    @else
+    <div class="col s11 m11 w3-border-t radius white w3-margin-left">
         <div class="row">
-            <div class="col s12 m4">
-                <table id="myTable" class="w3-table" style="font-size: 13px !important; margin:5px">
-                    <tr class="teal">
-                        <th>subjects</th>
-                        <th>coefficient</th>
-                        <th>average points</th>
-                        <th>Number of Students</th>
-                    </tr>
-                    @foreach ($subjects as $key => $subject)
-                        <tr>
-                            <td>{{ $subjects[$key]['subject_name'] }}</td>
-                            <td>{{ $subjects[$key]['subject_coff'] }}</td>
-                            <td>{{ $subjects[$key]['point'] }}</td>
-                            <td>{{ $subjects[$key]['number_student'] }}</td>
+            <div class="row">
+                <div class="col s12 m10 offset-m1">
+                    <table id="myTable" class="w3-table" style="font-size: 13px !important; margin:5px">
+                        <tr class="teal">
+                            <th>subjects</th>
+                            <th>coefficient</th>
+                            <th>average points</th>
+                            <th>Number of Students</th>
                         </tr>
-                    @endforeach
-                    <tr class="teal-text center" style="background-color: rgb(169, 238, 238)">
-                        <td>Total</td>
-                        <td class="bold">{{ $sum_coff }}</td>
-                        <td class="bold">{{ $total_point }}</td>
-                        <td class="bold"></td>
-                    </tr>
-                </table>
+                        @foreach ($subjects as $key => $subject)
+                            <tr>
+                                <td>{{ $subjects[$key]['subject_name'] }}</td>
+                                <td>{{ $subjects[$key]['subject_coff'] }}</td>
+                                <td>{{ $subjects[$key]['point'] }}</td>
+                                <td>{{ $subjects[$key]['number_student'] }}</td>
+                            </tr>
+                        @endforeach
+                        <tr class="teal-text center" style="background-color: rgb(169, 238, 238)">
+                            <td>Total</td>
+                            <td class="bold">{{ $sum_coff }}</td>
+                            <td class="bold">{{ $total_point }}</td>
+                            <td class="bold">{{ $total_student}}</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
-            <div class="col hide-on-med-and-down" style="margin-top: 150px; margin-left:10px; position: relative;"><i class="fa fa-arrow-alt-circle-right w3-xlarge w3-center teal-text"></i></div>
-            <div class="col s12 m7">
-                <table id="myTable" class="w3-table" style="font-size: 13px !important; margin:5px">
-                    <tr class="teal">
-                        <th>subjects</th>
-                        <th>coefficient</th>
-                        <th>average points</th>
-                        <th>Number of Students</th>
-                    </tr>
-                    @foreach ($subjects as $key => $subject)
-                        <tr>
-                            <td>{{ $subjects[$key]['subject_name'] }}</td>
-                            <td>{{ $subjects[$key]['subject_coff'] }}</td>
-                            <td>{{ $subjects[$key]['point'] }}</td>
-                            <td>{{ $subjects[$key]['number_student'] }}</td>
+
+            <div class="row">
+                <div class="col s12 m12" style="overflow-x: auto">
+                    <table id="myTable" class="w3-table" style="font-size: 13px !important; margin:5px">
+                        <tr class="teal">
+                            <th rowspan="2">subjects</th>
+                            <th colspan="6">{{ $seq_name1 }}</th>
+                            <th colspan="6">{{ $seq_name2 }}</th>
                         </tr>
-                    @endforeach
-                    <tr class="teal-text center" style="background-color: rgb(169, 238, 238)">
-                        <td>Total</td>
-                        <td class="bold">{{ $sum_coff }}</td>
-                        <td class="bold">{{ $total_point }}</td>
-                        <td class="bold"></td>
-                    </tr>
-                </table>
+
+                        <tr>
+                            <td>Students</td>
+                            <td>took test</td>
+                            <td>passed</td>
+                            <td>highest Mark</td>
+                            <td>Percetage /100</td>
+                            <td>Average /20</td>
+
+                            <td>Students</td>
+                            <td>toke test</td>
+                            <td>passed</td>
+                            <td>highest Mark</td>
+                            <td>Percetage /100</td>
+                            <td>Average /20</td>
+                        </tr>
+
+                        @foreach ($term as $key => $item)
+                        <tr>
+                            <td>{{ $term[$key]['sub_name'] }}</td>
+
+                            <td>{{ $term[$key]['first_test_student'] }}</td>
+                            <td>{{ $term[$key]['total_wrote'] }}</td>
+                            <td>{{ $term[$key]['total_pass'] }}</td>
+                            <td>{{ $term[$key]['highest_mark'] }}</td>
+                            <td @if($term[$key]['percentage']  < 50) style="color:#F44336" @else  style="color:#2196F3"  @endif>{{ $term[$key]['percentage'] }} %</td>
+                            <td @if($term[$key]['average']  < 10) style="color:#F44336" @else  style="color:#2196F3"  @endif>{{ $term[$key]['average'] }}</td>
+
+
+                            <td>{{ $terms[$key]['second_test_student'] }}</td>
+                            <td>{{ $terms[$key]['total_wrote_two'] }}</td>
+                            <td>{{ $terms[$key]['total_pass_two'] }}</td>
+                            <td>{{ $terms[$key]['highest_mark_two'] }}</td>
+                            <td @if($terms[$key]['percentage_two']  < 50) style="color:#F44336" @else  style="color:#2196F3"  @endif>{{ $terms[$key]['percentage_two'] }}</td>
+                            <td @if($terms[$key]['average_two']  < 50) style="color:#F44336" @else  style="color:#2196F3"  @endif>{{ $terms[$key]['average_two'] }}</td>
+                        </tr>
+                        @endforeach
+
+                        <tr class="teal-text center" style="background-color: rgb(169, 238, 238)">
+                            <td>Total</td>
+
+                            <td class="bold">{{ $term[$key]['total_student'] }}</td>
+                            <td class="bold"></td>
+                            <td class="bold"></td>
+                            <td class="bold"></td>
+                            <td class="bold" @if($term[$key]['total_percent']  < 50) style="color:red" @else  style="color:blue"  @endif>{{ $term[$key]['total_percent'] }}</td>
+                            <td class="bold" @if($term[$key]['total_average']  < 10) style="color:red" @else  style="color:blue"  @endif>{{ $term[$key]['total_average'] }}</td>
+
+                            <td class="bold">{{ $terms[$key]['total_student_two'] }}</td>
+                            <td class="bold"></td>
+                            <td class="bold"></td>
+                            <td class="bold"></td>
+                            <td class="bold" @if($terms[$key]['total_percent_two']  < 50) style="color:red" @else  style="color:blue"  @endif>{{ $terms[$key]['total_percent_two'] }}</td>
+                            <td class="bold" @if($terms[$key]['total_average_two']  < 10) style="color:red" @else  style="color:blue"  @endif>{{ $terms[$key]['total_average_two'] }}</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
+    @endif
 </div>
 
 @endsection
+
